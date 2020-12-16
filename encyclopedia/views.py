34b954720bf422de3c0a964bc672b1848e51fd66
Breno Holanda Saloio <brenosaloio@Brenos-MacBook-Air.local>
2020-12-16 00:35:32 -0300

    New Page using python form

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from django import forms
 
 from . import util
+
+
+
+
+class ContentForm(forms.Form):
+    entry_title = forms.CharField()
+    entry_content = forms.CharField(widget=forms.Textarea(attrs={'width':"50%", 'cols' : "10", 'rows': "20", }))
+
 
 
 def index(request):
@@ -10,5 +19,6 @@ def index(request):
 
 def new_page(request):
     return render(request, "encyclopedia/new_page.html", {
-        "entries": util.list_entries()
+        "entries": util.list_entries(),
+        "form": ContentForm()
     })
